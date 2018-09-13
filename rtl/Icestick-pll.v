6e1e5650100e78741ca8aa313eca7afb990c6f47
Steve White <swhite@lekker.local>
2018-09-13 10:46:16 -0700

    Drop PLL to 25MHz per icetime output.

--- rtl/Icestick-pll.v
+++ rtl/Icestick-pll.v
@@ -6,8 +6,8 @@
  * Use at your own risk.
  *
  * Given input frequency:        12.000 MHz
- * Requested output frequency:   40.000 MHz
- * Achieved output frequency:    39.750 MHz
+ * Requested output frequency:   25.000 MHz
+ * Achieved output frequency:    25.125 MHz
  */
 
 module pll(
@@ -19,8 +19,8 @@ module pll(
 SB_PLL40_CORE #(
 		.FEEDBACK_PATH("SIMPLE"),
 		.DIVR(4'b0000),		// DIVR =  0
-		.DIVF(7'b0110100),	// DIVF = 52
-		.DIVQ(3'b100),		// DIVQ =  4
+		.DIVF(7'b1000010),	// DIVF = 66
+		.DIVQ(3'b101),		// DIVQ =  5
 		.FILTER_RANGE(3'b001)	// FILTER_RANGE = 1
 	) uut (
 		.LOCK(locked),
