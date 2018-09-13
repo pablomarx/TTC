35f98bcf75209dd5b682eaaf06c15b8680319c5c
Steve White <swhite@lekker.local>
2018-09-12 21:42:55 -0700

    Add modules for
    1) instruction memory (ramx), which preloads prom.mem
    2) data memory (ramw)
    3) register files (ramz)

---
+++ rtl/ramw.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps 
+module ramw(
+	input clka,
+	input [9:0] addra,
+	input wea,
+	input [31:0] dina,
+	
+	input clkb,
+	input [9:0] addrb,
+	output reg [31:0] doutb
+);
+
+reg [31:0] mem[10:0];
+
+always @(posedge clka) begin
+	if (wea) begin
+		mem[addra] <= dina;
+	end
+end
+
+always @(posedge clkb) begin
+	doutb <= mem[addrb];
+end
+
+endmodule
+ 
---
+++ rtl/ramx.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps 
+module ramx(
+	input clka,
+	input [9:0] addra,
+	input wea,
+	input [31:0] dina,
+	
+	input clkb,
+	input [9:0] addrb,
+	output reg [31:0] doutb
+);
+
+reg [31:0] mem[10:0];
+
+initial begin
+	$readmemh("prom.mem", mem);
+end
+
+always @(posedge clka) begin
+	if (wea) begin
+		mem[addra] <= dina;
+	end
+end
+
+always @(posedge clkb) begin
+	doutb <= mem[addrb];
+end
+
+endmodule
+ 
